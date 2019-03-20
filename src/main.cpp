d5d9e398d9204483792133b7d36f926f7f372143
Student <student@ib.polsl.pl>
2019-03-20 14:09:59 +0100

    Zadanie trzecie

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,25 @@
-#include "trygonometria.h"
+#include "LaborkaConfig.h"
 #include <iostream>
+
+#ifdef USE_TRIGONOMETRY_DEGREE
+	#include "trygonometria.h"
+#endif
 
 int main()
 {
+#ifdef USE_TRIGONOMETRY_DEGREE
 	double sinus = degreemath::sin(30);
 	double cosinus = degreemath::cos(30);
 	double tangens = degreemath::tan(30);
 	double cotan = degreemath::ctg(30);
-
-	std::cout << sinus;
-	std::cout << cosinus;
-	std::cout << tangens;
-	std::cout << cotan;
+#else
+	double sinus = sin(30);
+	double cosinus = cos(30);
+	double tangens = tan(30);
+	double cotan = 1/tan(30);
+#endif
+	std::cout << sinus << std:endl;
+	std::cout << cosinus << std:endl;
+	std::cout << tangens << std:endl;
+	std::cout << cotan << std:endl;
 }
